230709bd7e09ea21966137bf1d376db6392c294c
titus <comptechs99@gmail.com>
2019-03-08 14:34:31 +0300

    country issue2 change commit

--- application/views/admin/clients/groups/profile.php
+++ application/views/admin/clients/groups/profile.php
@@ -192,7 +192,13 @@
   <input type="text" id="identity_type" name="identity_type" class="form-control"  value="">
   </div>
   </div>
- 
+  <div class="col-md-6">
+  <div class="form-group"> 
+  <label for="country_id_issued" class="control-label">
+  <small class="req text-danger"></small>Country of ID Issue</label>
+  <input type="text" id="country_id_issued" name="country_id_issued" class="form-control" value="">
+  </div>
+  </div>
   <div class="col-md-6">
   <div class="form-group">
   <label for="identity_number" class="control-label">
